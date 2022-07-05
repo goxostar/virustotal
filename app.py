292796555309c6db7e68f6b3058ea2135ccf66a7
goxostar <goktugekinci44@gmail.com>
2022-07-05 11:26:07 +0300

    declare limits and home page

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 
-
 app = Flask(__name__)
 
+FREE_DAILY_LIMIT = 500
+FREE_RATE = 4
+FREE_RATE_MINUTE = 60
+
 @app.route("/", methods=["GET", "POST"])
-def home():
-    return "<p>Hello, World!</p>"
+def home():    
+    return render_template("home.html", FREE_DAILY_LIMIT=FREE_DAILY_LIMIT)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
