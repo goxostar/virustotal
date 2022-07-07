3305cf9db0e78b527cd1af0b9bc56038acb8c447
goxostar <goktugekinci44@gmail.com>
2022-07-07 10:29:16 +0300

    url scan implemented

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import time
 import threading
 from ratelimit import limits, RateLimitException, sleep_and_retry
 from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from sqlalchemy import false, true
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 from werkzeug.utils import secure_filename
 import hashlib
 
@@ -32,6 +36,9 @@ def resetFreeDailyLimit():
 # Store Hash of Files to prevent redundant upload
 already_uploaded = {}
 
+# Store already scanned urls
+already_scanned_url = {}
+
 # Get the hash value of a file
 def sha256sum(filename):
     h  = hashlib.sha256()
@@ -45,6 +52,11 @@ def sha256sum(filename):
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+class UrlForm(FlaskForm):
+  url = StringField(validators=[InputRequired(), Length(min=1, max=100)], render_kw={"placeholder": "Url"})
+
+  submit = SubmitField('Lookup')
 
 @app.route('/file', methods=['GET', 'POST'])
 def file():
@@ -122,7 +134,88 @@ def filescan():
             return response.json()  
         else:
             return "Daily limit Reached"    
+
+@app.route('/url', methods=['GET','POST'])
+def url():
+
+    form = UrlForm()
+
+    if form.validate_on_submit():        
+            
+        # Login Request with User Input
+        url=form.url.data  
+        return redirect(url_for('urlscan', url=url))                 
+
+    return render_template('url.html',form=form)
     
+@app.route('/urlscan', methods=['GET','POST'])
+@sleep_and_retry
+@limits(calls=FREE_RATE, period=FREE_RATE_MINUTE)
+def urlscan():
+
+    global FREE_DAILY_LIMIT
+
+    if request.args.get('url') is None:
+        return redirect(url_for('url'))
+    
+    urlname = request.args.get('url')
+
+    # Url Validator
+    # Check if url is valid
+    prefixes = ["https://", "http://"]
+    def validate(url):
+        for pre in prefixes:
+            url = url.strip(pre)
+            if url.startswith("www"):
+                return true
+            else:
+                return false
+    
+    if validate(urlname) != true:
+        return redirect(url_for('url'))                
+
+    # URL Request
+    url_scan = "https://www.virustotal.com/api/v3/urls"
+    payload = "url={}".format(urlname)
+    headers_url = {
+    "Accept": "application/json",
+    "x-apikey": "542883fc18664cc7ae3dab65b8245384b08386329ec29e43ebaa6511526e7673",
+    "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    if urlname in already_scanned_url:
+        if FREE_DAILY_LIMIT>0:
+            analysis_id = already_scanned_url[urlname]
+            # Analysis Request  
+            url_analysis = "https://www.virustotal.com/api/v3/urls/{}".format(analysis_id)
+            headers_analysis = {
+                "Accept": "application/json",
+                "x-apikey": "542883fc18664cc7ae3dab65b8245384b08386329ec29e43ebaa6511526e7673"
+            }
+            response = requests.get(url_analysis, headers=headers_analysis)
+            FREE_DAILY_LIMIT = FREE_DAILY_LIMIT - 1
+            return response.json()
+        else:
+            return "Daily limit reached"
+    else:
+        if FREE_DAILY_LIMIT>0:
+            response = requests.post(url_scan, data=payload, headers=headers_url)
+            FREE_DAILY_LIMIT = FREE_DAILY_LIMIT - 1
+            # Get url analysis id from response            
+            analysis_id = response.json()['data']['id'].split("-")[1]
+            # Analysis Request  
+            url_analysis = "https://www.virustotal.com/api/v3/urls/{}".format(analysis_id)
+            headers_analysis = {
+                "Accept": "application/json",
+                "x-apikey": "542883fc18664cc7ae3dab65b8245384b08386329ec29e43ebaa6511526e7673"
+            }       
+            response = requests.get(url_analysis, headers=headers_analysis)
+            FREE_DAILY_LIMIT = FREE_DAILY_LIMIT - 1
+            already_scanned_url[urlname] = analysis_id            
+            return response.json()  
+        else:
+            return "Daily limit Reached"
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():    
